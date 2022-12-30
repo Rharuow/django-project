da60d0f14da89cdfb01f63973978635cf8ee2a7a
Harysson <haryssonsoares@gmail.com>
2022-12-30 19:37:10 -0300

    context inside templates

---
+++ recipes/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'recipes/home.html', context={'name': 'Harysson Soares'})
+
+
+def about(request):
+    return HttpResponse("Sobre")
+
+
+def contact(request):
+    return HttpResponse("Contato")
